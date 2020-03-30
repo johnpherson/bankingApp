2fcb7f0ca1f1df775068c9b52027421f3694917a
johnpherson <phersonj@gmail.com>
2020-03-29 20:40:20 -0600

    first commit

---
+++ src/scannerTest.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class scannerTest {
+    public static void main(String[] args) {
+        //TODO Auto-generate method sub
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a charcter ");
+        //int a = scanner.nextInt();
+        char c = scanner.next().charAt(0);
+        System.out.println("Value = "+c);
+
+
+        //System.out.println("value = "+a);
+
+
+    }
+}
